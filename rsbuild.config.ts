450aac53290ac19e2ec3d0a2a3f103e66730c3db
cc <44751363+zzfn@users.noreply.github.com>
2024-07-25 12:30:52 +0800

    feat: init

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  html: {
+    title: 'xx',
+  },
   output: {
     assetPrefix: 'https://cdn.zzfzzf.com/xx/',
   },
