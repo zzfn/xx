904bb3f1a2d56b1de22a4f3b05b48b730a5f0d07
cc <44751363+zzfn@users.noreply.github.com>
2024-07-25 13:05:45 +0800

    feat: favicon

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   html: {
-    title: 'xx',
+    favicon: 'https://cdn.zzfzzf.com/assets/favicon.ico',
+    title: '寻仙攻略',
   },
   output: {
     assetPrefix: 'https://cdn.zzfzzf.com/xx/',
