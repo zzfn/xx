5bd197707723c60cfabc9aecd28679d596a15835
cc <44751363+zzfn@users.noreply.github.com>
2024-08-07 16:34:38 +0800

    feat: router

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,19 +6,19 @@ const Home = () => {
     const [steps, setSteps] = useState<string[]>([]);
     const [attributes, setAttributes] = useState('');
     const [cost, setCost] = useState(0);
-    const handleCalculate = () => {
+    const handleCalculate = (target:number) => {
         if (attributes.length !== 4) {
             alert('请输入四位数');
             return;
         }
-        const result = calculateCost(attributes);
+        const result = calculateCost(attributes,target);
         setCost(result.cost);
         setSteps(result.steps);
     };
 
     return (
         <div className='min-h-screen p-6'>
-            <h1 className='text-xl text-center my-2'>寻仙圣宠升级到 8888 计算器</h1>
+            <h1 className='text-xl text-center my-2'>寻仙圣宠升级到 8888/9999 计算器</h1>
             <div>
                 <div className="overflow-x-auto">
                     <table className="table table-xs">
@@ -44,30 +44,44 @@ const Home = () => {
                 </div>
                 <p role="alert" className="alert">共计所需{cost}仙玉</p>
                 <div className="divider"></div>
-                <input
-                    placeholder={'宝宝四维，如6688'}
-                    className='input input-bordered'
-                    minLength={4}
-                    maxLength={4}
-                    value={attributes}
-                    onInput={(event: ChangeEvent<HTMLInputElement>) => {
-                        const attributes = event.target.value;
-                        setAttributes(attributes);
-                    }}
-                    type='text'
-                />
-                <button
-                    className='btn'
-                    onClick={() => {
-                        if (attributes.length !== 4) {
-                            alert('请输入四位数');
-                            return;
-                        }
-                        handleCalculate();
-                    }}
-                >
-                    计算
-                </button>
+                <div className='flex gap-x-2'>
+                    <input
+                        placeholder={'宝宝四维，如6688'}
+                        className='input input-bordered'
+                        minLength={4}
+                        maxLength={4}
+                        value={attributes}
+                        onInput={(event: ChangeEvent<HTMLInputElement>) => {
+                            const attributes = event.target.value;
+                            setAttributes(attributes);
+                        }}
+                        type='text'
+                    />
+                    <button
+                        className='btn'
+                        onClick={() => {
+                            if (attributes.length !== 4) {
+                                alert('请输入四位数');
+                                return;
+                            }
+                            handleCalculate(8);
+                        }}
+                    >
+                        计算升级到8888
+                    </button>
+                    <button
+                        className='btn'
+                        onClick={() => {
+                            if (attributes.length !== 4) {
+                                alert('请输入四位数');
+                                return;
+                            }
+                            handleCalculate(9);
+                        }}
+                    >
+                        计算升级到9999
+                    </button>
+                </div>
             </div>
             规则如下：
             <p>融魄精元 1400仙玉/个</p>
--- src/calculateCost.ts
+++ src/calculateCost.ts
@@ -1,4 +1,4 @@
-export function calculateCost(attributes: string) {
+export function calculateCost(attributes: string,target:number) {
   let cost = 0;
   let steps:string[] = []; // Store steps here
   let attrArray = attributes.split('').map(Number);
@@ -36,7 +36,7 @@ export function calculateCost(attributes: string) {
         const eightsCount = arr.filter(num => num >= 8).length;
         if (eightsCount === 0) {
           cost += 2800;
-          steps.push(`第二个7升8，花费 2800 仙玉`);
+          steps.push(`第一个7升8，花费 2800 仙玉`);
         } else if (eightsCount === 1) {
           cost += 5600;
           steps.push(`第二个7升8，花费 5600 仙玉`);
@@ -51,9 +51,31 @@ export function calculateCost(attributes: string) {
         return;
       }
     }
+    if(target ===9){
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 9) {
+          const nineCount = arr.filter(num => num >= 9).length;
+          if (nineCount === 0) {
+            cost += 5600;
+            steps.push(`第一个8升9，花费 5600 仙玉`);
+          } else if (nineCount === 1) {
+            cost += 11200;
+            steps.push(`第二个8升9，花费 11200 仙玉`);
+          } else if (nineCount === 2) {
+            cost += 44800;
+            steps.push(`第三个8升9，花费 44800 仙玉`);
+          } else if (nineCount === 3) {
+            cost += 50400;
+            steps.push(`第四个8升9，花费 50400 仙玉`);
+          }
+          arr[i]++;
+          return;
+        }
+      }
+    }
   };
 // 8升9 4 8 32 36
-  while (attrArray.some(num => num < 8)) {
+  while (attrArray.some(num => num < target)) {
     upgrade(attrArray);
   }
 
