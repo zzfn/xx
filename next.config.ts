f0a2f2bf48aac85a17dc9e8d3b3e61d2c4fab836
cc <44751363+zzfn@users.noreply.github.com>
2024-11-25 17:35:33 +0800

    build(next-config): 配置 Next.js 项目
    - 添加 standalone 输出模式
    - 配置资产前缀以支持生产环境
    -启用 React 严格模式
    - 配置图像优化选项
    - 设置实验性服务器操作的允许来源

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone',
+    assetPrefix:
+        process.env.NODE_ENV === 'production' && !process.env.VERCEL
+            ? 'https://cdn.zzfzzf.com/xx'
+            : '/',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+        formats: ['image/avif', 'image/webp'],
+    },
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['zzfzzf.com', '*.ooxo.cc', 'ccw.es'],
+        },
+    },
+    productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
