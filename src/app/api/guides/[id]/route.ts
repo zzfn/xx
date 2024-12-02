946ad7f318d17999054e5f138ed9168cf0790692
cc <44751363+zzfn@users.noreply.github.com>
2024-12-02 15:32:48 +0800

    refactor(app): 优化 guides API 路由
    - 更新 GET 函数以接收 params 参数
    - 调整 console.log 位置以提高可读性

--- src/app/api/guides/[id]/route.ts
+++ src/app/api/guides/[id]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-  console.log(111,request);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
+    console.log(111,request);
     const response = await fetch(`https://api.ccw.es/v1/articles/${params.id}`);
     const data = await response.json();
     
