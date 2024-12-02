c467d69671c5e2d5735ecf048c7b5555baceec27
cc <44751363+zzfn@users.noreply.github.com>
2024-12-02 15:36:24 +0800

    refactor(app): 重构 guides API 路由
    - 优化了 GET 函数的参数处理，支持异步获取 params
    - 移除了不必要的 console.log 语句
    - 添加了路由路径的注释

--- src/app/api/guides/[id]/route.ts
+++ src/app/api/guides/[id]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+// /api/guides/[id]
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
   try {
     console.log(111,request);
-    const response = await fetch(`https://api.ccw.es/v1/articles/${params.id}`);
+    const id = (await params).id
+    const response = await fetch(`https://api.ccw.es/v1/articles/${id}`);
     const data = await response.json();
     
     return NextResponse.json(data);
