a9dd3a27a694ba341c5374ed0a2f71034b716e8f
cc <44751363+zzfn@users.noreply.github.com>
2024-12-02 15:58:50 +0800

    build(next-config): 更新 Next.js 配置文件
    - 调整代码格式，使用一致的双引号
    - 添加 ESLint 配置，忽略构建期间的 ESLint 错误
    - 优化配置文件的结构布局

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,27 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'standalone',
-    assetPrefix:
-        process.env.NODE_ENV === 'production' && !process.env.VERCEL
-            ? 'https://cdn.zzfzzf.com/xx'
-            : '/',
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-        formats: ['image/avif', 'image/webp'],
+  output: "standalone",
+  assetPrefix:
+    process.env.NODE_ENV === "production" && !process.env.VERCEL
+      ? "https://cdn.zzfzzf.com/xx"
+      : "/",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["zzfzzf.com", "*.ooxo.cc", "ccw.es"],
     },
-    experimental: {
-        serverActions: {
-            allowedOrigins: ['zzfzzf.com', '*.ooxo.cc', 'ccw.es'],
-        },
-    },
-    productionBrowserSourceMaps: true,
+  },
+  productionBrowserSourceMaps: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
